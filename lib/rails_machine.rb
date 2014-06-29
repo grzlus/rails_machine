dc775335b1fa9865167b176cc1ae0facbf679a37
Grzegorz Łuszczek <grzegorz@piklus.pl>
2014-06-29 11:03:23 +0200

    Fix tabs

--- lib/rails_machine.rb
+++ lib/rails_machine.rb
@@ -1,40 +1,40 @@
 require 'rails_machine/configuration'
 
 module RailsMachine
-	extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-	included do
-		cattr_accessor :transitions
+  included do
+    cattr_accessor :transitions
 
-		validate :allowed_transition, if: :state_changed?
-	end
+    validate :allowed_transition, if: :state_changed?
+  end
 
-	def allowed_transition
-		from = self.state_was.to_sym
-		to = self.state.to_sym
+  def allowed_transition
+    from = self.state_was.to_sym
+    to = self.state.to_sym
 
-		transitions = (transitions_for(from) + transitions_for(:any)).select { |t| t[:to] == to || t[:to] == :any }
-		return errors.add(:state, :transition_not_found) if transitions.empty?
+    transitions = (transitions_for(from) + transitions_for(:any)).select { |t| t[:to] == to || t[:to] == :any }
+    return errors.add(:state, :transition_not_found) if transitions.empty?
 
-		errors.add(:state, :guard_failed) if transitions.none? { |t| t[:guards].all? { |guard| guard.call(self) } }
-	end
+    errors.add(:state, :guard_failed) if transitions.none? { |t| t[:guards].all? { |guard| guard.call(self) } }
+  end
 
-	module ClassMethods
-		def rails_machine(column: :state, &blk)
-			raise ArgumentError unless block_given?
+  module ClassMethods
+    def rails_machine(column: :state, &blk)
+      raise ArgumentError unless block_given?
 
-			configuration = Configuration.new
-			configuration.run(&blk)
+      configuration = Configuration.new
+      configuration.run(&blk)
 
-			self.transitions = configuration.transitions
+      self.transitions = configuration.transitions
 
-			enum column => Hash[configuration.states]
-		end
-	end
+      enum column => Hash[configuration.states]
+    end
+  end
 
-	protected
+  protected
 
-	def transitions_for(action)
-		self.class.transitions[action] || []
-	end
+  def transitions_for(action)
+    self.class.transitions[action] || []
+  end
 end
